e6e23bd2c046ff00659f7595ef8feaa9da3cdbaf
Yana Yakovleva <40498328+YanaYakovleva2018@users.noreply.github.com>
2018-06-25 19:35:25 +0300

    git add

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Adding = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Button();
+            this.Sub = new System.Windows.Forms.Button();
+            this.Mul = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Reverse = new System.Windows.Forms.Button();
+            this.Cos = new System.Windows.Forms.Button();
+            this.HalfPow = new System.Windows.Forms.Button();
+            this.Divine = new System.Windows.Forms.Button();
+            this.Error = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 38);
+            this.textBox1.MaxLength = 4000000;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(45, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ввод данных";
+            // 
+            // Adding
+            // 
+            this.Adding.BackColor = System.Drawing.Color.DeepPink;
+            this.Adding.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Adding.Location = new System.Drawing.Point(30, 31);
+            this.Adding.Name = "Adding";
+            this.Adding.Size = new System.Drawing.Size(75, 23);
+            this.Adding.TabIndex = 8;
+            this.Adding.Text = "+";
+            this.Adding.UseVisualStyleBackColor = false;
+            this.Adding.Click += new System.EventHandler(this.Adding_Click);
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(159, 119);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(75, 23);
+            this.Result.TabIndex = 9;
+            this.Result.Text = "=";
+            this.Result.UseVisualStyleBackColor = true;
+            this.Result.Click += new System.EventHandler(this.Result_Click);
+            // 
+            // Sub
+            // 
+            this.Sub.BackColor = System.Drawing.Color.Turquoise;
+            this.Sub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sub.Location = new System.Drawing.Point(159, 31);
+            this.Sub.Name = "Sub";
+            this.Sub.Size = new System.Drawing.Size(75, 23);
+            this.Sub.TabIndex = 10;
+            this.Sub.Text = "-";
+            this.Sub.UseVisualStyleBackColor = false;
+            this.Sub.Click += new System.EventHandler(this.Sub_Click);
+            // 
+            // Mul
+            // 
+            this.Mul.BackColor = System.Drawing.Color.Blue;
+            this.Mul.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Mul.Location = new System.Drawing.Point(30, 61);
+            this.Mul.Name = "Mul";
+            this.Mul.Size = new System.Drawing.Size(75, 23);
+            this.Mul.TabIndex = 11;
+            this.Mul.Text = "*";
+            this.Mul.UseVisualStyleBackColor = false;
+            this.Mul.Click += new System.EventHandler(this.Mul_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.groupBox1.Controls.Add(this.Reverse);
+            this.groupBox1.Controls.Add(this.Cos);
+            this.groupBox1.Controls.Add(this.HalfPow);
+            this.groupBox1.Controls.Add(this.Divine);
+            this.groupBox1.Controls.Add(this.Mul);
+            this.groupBox1.Controls.Add(this.Sub);
+            this.groupBox1.Controls.Add(this.Result);
+            this.groupBox1.Controls.Add(this.Adding);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Navy;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.groupBox1.Location = new System.Drawing.Point(33, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 171);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Основные кнопки";
+            // 
+            // Reverse
+            // 
+            this.Reverse.BackColor = System.Drawing.Color.OrangeRed;
+            this.Reverse.Location = new System.Drawing.Point(30, 118);
+            this.Reverse.Name = "Reverse";
+            this.Reverse.Size = new System.Drawing.Size(75, 23);
+            this.Reverse.TabIndex = 15;
+            this.Reverse.Text = "1/x";
+            this.Reverse.UseVisualStyleBackColor = false;
+            this.Reverse.Click += new System.EventHandler(this.Reverse_Click);
+            // 
+            // Cos
+            // 
+            this.Cos.BackColor = System.Drawing.Color.Pink;
+            this.Cos.Location = new System.Drawing.Point(159, 90);
+            this.Cos.Name = "Cos";
+            this.Cos.Size = new System.Drawing.Size(75, 23);
+            this.Cos.TabIndex = 14;
+            this.Cos.Text = "Cos(x)";
+            this.Cos.UseVisualStyleBackColor = false;
+            this.Cos.Click += new System.EventHandler(this.Cos_Click);
+            // 
+            // HalfPow
+            // 
+            this.HalfPow.BackColor = System.Drawing.Color.Crimson;
+            this.HalfPow.Location = new System.Drawing.Point(30, 91);
+            this.HalfPow.Name = "HalfPow";
+            this.HalfPow.Size = new System.Drawing.Size(75, 23);
+            this.HalfPow.TabIndex = 13;
+            this.HalfPow.Text = "1^1/2";
+            this.HalfPow.UseVisualStyleBackColor = false;
+            this.HalfPow.Click += new System.EventHandler(this.HalfPow_Click);
+            // 
+            // Divine
+            // 
+            this.Divine.BackColor = System.Drawing.Color.Lime;
+            this.Divine.Location = new System.Drawing.Point(159, 60);
+            this.Divine.Name = "Divine";
+            this.Divine.Size = new System.Drawing.Size(75, 23);
+            this.Divine.TabIndex = 12;
+            this.Divine.Text = "/";
+            this.Divine.UseVisualStyleBackColor = false;
+            this.Divine.Click += new System.EventHandler(this.Divine_Click);
+            // 
+            // Error
+            // 
+            this.Error.AutoSize = true;
+            this.Error.BackColor = System.Drawing.Color.Pink;
+            this.Error.ForeColor = System.Drawing.Color.Black;
+            this.Error.Location = new System.Drawing.Point(45, 65);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(0, 13);
+            this.Error.TabIndex = 13;
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.Color.DarkRed;
+            this.Close.ForeColor = System.Drawing.Color.Black;
+            this.Close.Location = new System.Drawing.Point(349, 26);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(106, 35);
+            this.Close.TabIndex = 14;
+            this.Close.Text = "Закрыть";
+            this.Close.UseVisualStyleBackColor = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Clear.Location = new System.Drawing.Point(349, 100);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(106, 35);
+            this.Clear.TabIndex = 15;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Подсказка: Пределы вводимых чисел от -2000000 до 4000000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(477, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Error);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Adding;
+        private System.Windows.Forms.Button Result;
+        private System.Windows.Forms.Button Sub;
+        private System.Windows.Forms.Button Mul;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Divine;
+        private System.Windows.Forms.Label Error;
+        private System.Windows.Forms.Button HalfPow;
+        private System.Windows.Forms.Button Cos;
+        private System.Windows.Forms.Button Reverse;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Экземпляр класса эмулирующего  работу калькулятора
+        /// </summary>
+        Calc Device = new Calc();
+        /// <summary>
+        /// Переменная, хранящая номер операции
+        /// </summary>
+        public int operation = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+        }
+        /// <summary>
+        /// Обработчик щёлчка по кнопке суммы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Adding_Click(object sender, EventArgs e)
+        {
+            if (operation == 0)
+            {
+                if (textBox1.Text != "")
+                {
+                    Device.st_op1 = textBox1.Text;
+                    if (Device.check1())
+                    {
+                        operation = 1;
+                    }
+                    else
+                    {
+                        Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                        Device.st_op1 = "";
+                    }
+                    textBox1.Text = "";
+
+                }
+                else
+                    Error.Text = "Введите первое число!";
+                textBox1.Focus();
+            }
+            else
+            {
+                Error.Text = "Операция уже выбранная!";
+                textBox1.Focus();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Обработчик щелчка по кнопке результата
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Result_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Device.st_op2 = textBox1.Text;
+                if (Device.check2())
+                {
+                    switch (operation)
+                    {
+                        case 0:
+                            {
+                                Error.Text = "Выберите операцию!!"; break;
+                            }
+                        case 1:
+                            {
+                                Device.Add();
+                                operation = 0;
+                                string tmp = Device.result.ToString("G4").Replace(',', '.');
+                                textBox1.Text = tmp; break;
+                            }
+                        case 2:
+                            {
+                                Device.Sub();
+                                operation = 0;
+                                string tmp = Device.result.ToString("G4").Replace(',', '.');
+                                textBox1.Text = tmp; break;
+                            }
+                        case 3:
+                            {
+                                Device.Mul();
+                                operation = 0;
+                                string tmp = Device.result.ToString("G4").Replace(',', '.');
+                                textBox1.Text = tmp; break;
+                            }
+                        case 4:
+                            {
+                                operation = 0;
+                                if (Device.Div())
+                                {
+                                    string tmp = Device.result.ToString("G4").Replace(',', '.');
+                                    textBox1.Text = tmp; break;
+                                }
+                                else
+                                {
+                                    Error.Text = "Попытка деления на ноль!!!"; break;
+                                }
+                            }
+
+
+                    }
+                }
+                else 
+                {
+                    Device.st_op2 = "";
+                    Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                }
+            }
+            else
+                Error.Text = "Введите второе число!";
+        }
+
+    
+        /// <summary>
+        /// Обработчик события щелчка по кнопке Вычитания
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Sub_Click(object sender, EventArgs e)
+        {
+            if (operation == 0)
+            {
+                if (textBox1.Text != "")
+                {
+                    Device.st_op1 = textBox1.Text;
+                    if (Device.check1())
+                    {
+                        operation = 2;
+                    }
+                    else
+                    {
+                        Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                        Device.st_op1 = "";
+                    }
+                    textBox1.Text = "";
+
+                }
+                else
+                    Error.Text = "Введите первое число!";
+                textBox1.Focus();
+            }
+            else 
+            {
+                Error.Text = "Операция уже выбранная!";
+                textBox1.Focus();
+            }
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке Умножения 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Mul_Click(object sender, EventArgs e)
+        {
+            if (operation == 0)
+            {
+                if (textBox1.Text != "")
+                {
+                    Device.st_op1 = textBox1.Text;
+                    if (Device.check1())
+                    {
+                        operation = 3;
+                    }
+                    else
+                    {
+                        Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                        Device.st_op1 = "";
+                    }
+                    textBox1.Text = "";
+
+                }
+                else
+                    Error.Text = "Введите первое число!";
+                textBox1.Focus();
+            }
+            else 
+            {
+                Error.Text = "Операция уже выбранная!";
+                textBox1.Focus();
+            }
+        }
+        /// <summary>
+        /// Обработчик первого появления формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке деления
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Divine_Click(object sender, EventArgs e)
+        {
+            if (operation == 0)
+            {
+                if (textBox1.Text != "")
+                {
+                    Device.st_op1 = textBox1.Text;
+                    if (Device.check1())
+                    {
+                        operation = 4;
+                    }
+                    else
+                    {
+                        Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                        Device.st_op1 = "";
+                    }
+                    textBox1.Text = "";
+
+                }
+                else
+                    Error.Text = "Введите первое число!";
+                textBox1.Focus();
+            }
+            else 
+            {
+                Error.Text = "Операция уже выбранная!";
+                textBox1.Focus();
+            }
+
+        }
+        /// <summary>
+        /// Обработчик, контролирующий работу клавиши Delete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                textBox1.Text = "";
+            }
+        }
+        /// <summary>
+        /// Обработчик, контролирующий ввод данных с клавиатуры
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Error.Text = "";
+            string pattern2  = @"^[\+-]?\d{1,9}.?\d{0,5}[eE][\+-]?\d{1,2}$";
+            string pattern = @"^[\+-]{0,1}[0-9]*(\d[.]){0,1}[0-9]{0,5}$";
+            if (e.KeyChar == (char)Keys.Back)
+            {
+                if(Regex.IsMatch(textBox1.Text, pattern2))
+                {
+                    Error.Text = "Этот формат не для форматирования!!!";
+                    e.KeyChar = (char)Keys.None;
+                }
+                else
+                {
+                        if ( textBox1.SelectionStart>1)
+                        {
+                         textBox1.Select(textBox1.SelectionStart, 0);
+                        }
+                }
+            }
+            else
+            {
+                string number = textBox1.Text + e.KeyChar;
+                if (!Regex.IsMatch(number, pattern))
+                {
+                    e.KeyChar = (char)Keys.None;
+                }
+            }
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке взятие корня
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HalfPow_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Device.st_op1 = textBox1.Text;
+                if (Device.check1())
+                {
+                    if (Device.SquareRoot())
+                    {
+                        string tmp = Device.result.ToString("G4").Replace(',', '.');
+                        textBox1.Text = tmp;
+                    }
+                    else
+                    {
+                        Error.Text = "Попытка взятия корня из отрицательного числа!!!";
+                    }
+                }
+                else
+                {
+                    Device.st_op1 = "";
+                    Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                }
+
+            }
+            else
+                Error.Text = "Введите Операнд!";
+
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке взятия косинуса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cos_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Device.st_op1 = textBox1.Text;
+                if (Device.check1())
+                {
+                    Device.Cosinus();
+                    string tmp = Device.result.ToString("G4").Replace(',', '.');
+                    textBox1.Text = tmp;
+                }
+                else
+                {
+                    Device.st_op1 = "";
+                    Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                }
+            }
+            else
+                Error.Text = "Введите операнд!";
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке взятие обратного к данному
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Reverse_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Device.st_op1 = textBox1.Text;
+                if (Device.check1())
+                {
+                    if (Device.Reverse())
+                    {
+                        string tmp = Device.result.ToString("G4").Replace(',', '.');
+                        textBox1.Text = tmp;
+                    }
+                    else
+                    {
+                        Error.Text = "Попытка деления на ноль!!!";
+                    }
+                }
+                else 
+                {
+                    Device.st_op1 = "";
+                    Error.Text = "Некоректный размер операнда  или форма!!! Введите заново!";
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                }
+
+            }
+            else
+                Error.Text = "Введите Операнд!";
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке выхода из приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// Обработчик события щелчка по кнопке очистки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
